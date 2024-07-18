ae4d940b4a16ceaab455bf1cd64b77715c250066
Letícia Vieira Gonçalves <90807534+LeticiaVieirg@users.noreply.github.com>
2024-07-17 20:34:19 -0400

    Update 01.c

--- Unidade_1/Lists/lista01/01.c
+++ Unidade_1/Lists/lista01/01.c
@@ -12,3 +12,13 @@ int fibonacci (int n ) {
 int main () {
 	fibonacci (15) ;
 }
+
+/*
+a) Depure o c´odigo para descobrir quantas vezes o fibonacci(5) ´e calculado (requisitado).
+R:  
+b) Descubra atrav´es da depura¸c˜ao, qual a maior quantidade de n´ıveis (frames) esse c´odigo alcan¸ca.
+R: 
+c) Depure o c´odigo at´e o momento que o primeiro fibonacci(14) ´e calculado e o execut´avel ir´a come¸car
+a calcular o fibonacci(13).
+R:
+*/
